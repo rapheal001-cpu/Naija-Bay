64aef6613beace684104833bee12ea82c356c4a6
raphtech404 <raphtech404@gmail.com>
2026-08-12 12:39:47 +0100

    First Commit

--- Backend/build.sh
+++ Backend/build.sh
@@ -4,6 +4,3 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py categories
-python manage.py nigeria_states
-python manage.py create_user --admin

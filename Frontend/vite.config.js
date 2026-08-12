fda2b5fed7168c52ed15a740cf9fb107a5f4ec2d
raphtech404 <raphtech404@gmail.com>
2026-08-12 03:38:39 +0100

    First Commit

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -12,7 +12,7 @@ plugins: [react(), tailwindcss()],
     // open: true,
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8000',
+        target: 'https://naija-bay-backend.onrender.com',
         changeOrigin: true,
       },
     },
